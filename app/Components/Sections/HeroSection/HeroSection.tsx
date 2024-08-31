ee65fb3585ef57b193838ac324fac608381c479c
nguyenkhang1710 <nguyenkhang.khang2019@gmail.com>
2024-08-31 08:59:14 +0700

    test

--- app/Components/Sections/HeroSection/HeroSection.tsx
+++ app/Components/Sections/HeroSection/HeroSection.tsx
@@ -5,6 +5,7 @@ function HeroSection() {
     <section className="relative h-full w-full">
       <div className="absolute w-full h-[100vh] top-0 flex justify-evenly z-[-1]">
         <div className="relative w-[0.5px] h-full bg-orange-500"></div>
+        <div className="relative w-[0.5px] h-full bg-orange-500"></div>
       </div>
     </section>
   );
