fec45e93302ede73f542e2b88a7505bdd2a1158d
nguyenkhang1710 <nguyenkhang.khang2019@gmail.com>
2024-09-20 17:46:43 +0700

    FAQ_SEC

--- app/utils/utils.tsx
+++ app/utils/utils.tsx
@@ -122,37 +122,37 @@ export const baseText = [
 export const faqs = [
   {
     question:
-      "How to Determine Your Non-Negotiables When Buying a Luxury Property",
+      "How to Determine Your Non-Negotiables When Buying a Luxury Property  ?",
     answer:
       "It’s essential to go into purchasing a new property with a list of must-haves, or non-negotiables, so your agent can more easily find houses that meet your needs and make your final decision-making process more streamlined. Determining what your list of non-negotiables might be, though, isn’t always so easy.",
   },
 
   {
-    question: "How much rent can I afford?",
+    question: "How much rent can I afford ?",
     answer:
       "To calculate how much you can spend on rent, you’ll need to know your gross income. Gross income, also called gross pay when it's on a paycheck, is an individual's total earnings before taxes or other deductions. This income comes from all sources, not just employment, and comes in the form of cash, property, or services. Once you’ve determined your gross income, you can then use the 30% or 50/30/20 rules to determine how much rent you can afford.",
   },
 
   {
-    question: "What is the 50/30/20 rule?",
+    question: "What is the 50/30/20 rule ?",
     answer:
       "One alternative to the 30% rule is the 50/30/20 rule. This rule divides your monthly net (after-tax) income into three spending categories: 50% for needs, 30% for wants, and 20% for savings or paying off debt.",
   },
 
   {
-    question: "How much is too much rent?",
+    question: "How much is too much rent ?",
     answer:
       "Spending more on rent for a nicer home or apartment is tempting when looking for a place to live. However, experts recommend that you shouldn’t spend more than 30% of your monthly income on rent.",
   },
 
   {
-    question: "How do you calculate your rent-to-income ratio?",
+    question: "How do you calculate your rent-to-income ratio ?",
     answer:
       "To calculate your rent-to-income ratio, divide your monthly rent payment by your monthly gross income. For example, if you pay $1,000 per month and your gross income is $5,000 per month, your rent-to-income ratio is 20%.",
   },
 
   {
-    question: "What percentage of my income should go to rent?",
+    question: "What percentage of my income should go to rent ?",
     answer:
       "As a rule of thumb, your monthly rent shouldn’t exceed 30% of your gross monthly income. This leaves 70% of your gross monthly income to cover other expenses.For example, if you make $50,000 per year and follow the “30% rule,” you’d have $15,000 annually - up to $1,250 per month - to spend on rent. This leaves $2,900 per month for all other expenses",
   },
